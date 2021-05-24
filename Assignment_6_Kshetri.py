85c93c2f4220c52df87059e1d90e9ed7d0e79213
Nikesh Kshetri <nikeshkshetri@Nikeshs-MacBook-Air.local>
2021-05-23 19:53:35 -0500

    this is the second time we are pusing the code

---
+++ Assignment_6_Kshetri.py
@@ -0,0 +1,19 @@
+#Nikesh Kshetri, Assignment 6, 04/25/2021
+#Defining function
+def milesToKilometer(miles):
+    kilometer = float(miles * 1.60934)
+    return kilometer
+
+#using while loop (try, except) to prompt user for input and simultaneously check if the input is correct 
+while True:
+    try: 
+        miles = float(input("please enter the miles you like to convert: "))
+        break
+    except ValueError:
+        print("No valid input, please enter it again: ")
+
+#printing the final output
+print("Total Kilometer for the given input miles: ", milesToKilometer(miles))
+print("Total Miles you entered: ", miles)
+print("VERSION II ")
+
